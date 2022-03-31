17e397c60eabafa950208221314d2a59ddc2401f
Sean P <84537830+cheffyp@users.noreply.github.com>
2022-03-31 10:06:12 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,58 +1,67 @@
 var drum;
 
-function playDrum(drumVar){
+function playDrum(drumVar) {
   switch (drumVar) {
     case "w":
       var tom1 = new Audio('sounds/tom-1.mp3');
       tom1.play();
-    break;
+      break;
 
     case "a":
       var tom2 = new Audio('sounds/tom-2.mp3');
       tom2.play();
-    break;
+      break;
 
     case "s":
       var tom3 = new Audio('sounds/tom-3.mp3');
       tom3.play();
-    break;
+      break;
 
     case "d":
       var tom4 = new Audio('sounds/tom-4.mp3');
       tom4.play();
-    break;
+      break;
 
     case "j":
       var snare = new Audio('sounds/snare.mp3');
       snare.play();
-    break;
+      break;
 
     case "k":
       var kick = new Audio('sounds/kick-bass.mp3');
       kick.play();
-    break;
+      break;
 
     case "l":
       var crash = new Audio('sounds/crash.mp3');
       crash.play();
-    break;
+      break;
 
-    default: console.log(drum);
+    default:
+      console.log(drum);
   }
 }
 
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
-  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-      drum = this.innerHTML;
-      playDrum(drum);
-      }
-  );
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    drum = this.innerHTML;
+    playDrum(drum);
+    buttonAnimation(drum);
+  });
 }
 
 addEventListener("keydown", function(event) {
-
-    drum = event.key;
-    playDrum(drum);
-
+  drum = event.key;
+  playDrum(drum);
+  buttonAnimation(drum);
 })
+
+
+function buttonAnimation(key) {
+  var activeButton = document.querySelector("." + key);
+  activeButton.classList.add("pressed");
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
